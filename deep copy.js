4db9701bc962822bfcd1f2f5736d533c02713647
LucasMdeJong <41298691+LucasMdeJong@users.noreply.github.com>
2018-07-17 02:38:26 +0200

    Update deep copy.js

--- deep copy.js
+++ deep copy.js
@@ -1,6 +1,6 @@
 function clone(obj){
 	let newObj=Object.assign({},obj);
-	for(let property in obj){//for(let property of Object.getOwnPropertyNames(obj)){
+	for(let property in obj){//for(let property of Object.getOwnPropertyNames(obj)){ if(!(property in obj)) continue;
 		if(Object.values(obj[property])!=0){
 			newObj[property]=clone(obj[property]);
 		};
