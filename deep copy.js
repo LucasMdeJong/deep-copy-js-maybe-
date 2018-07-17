d0c85ca790336dcbeccd0d246b3b9905b30b9389
LucasMdeJong <41298691+LucasMdeJong@users.noreply.github.com>
2018-07-17 04:12:05 +0200

    add class methods
    change Object.assgn({},obj) to new obj.constructor()

--- deep copy.js
+++ deep copy.js
@@ -1,5 +1,5 @@
 function clone(obj){
-	let newObj=Object.assign({},obj);
+	let newObj=new obj.constructor();
 	for(let property in obj){//for(let property of Object.getOwnPropertyNames(obj)){ if(!(property in obj)) continue;
 		try{
 			Object.values(obj[property]);
