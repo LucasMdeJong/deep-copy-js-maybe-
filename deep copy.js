7f3d214a9f685cb671643319e0f5cd77badcfb00
LucasMdeJong <41298691+LucasMdeJong@users.noreply.github.com>
2018-07-17 04:16:04 +0200

    Update deep copy.js
    isn't coppying properties

--- deep copy.js
+++ deep copy.js
@@ -4,10 +4,13 @@ function clone(obj){
 		try{
 			Object.values(obj[property]);
 		} catch(err) {
+			newObj[property]=obj[property];
 			continue;
 		};
 		if(Object.values(obj[property]).length!==0){
 			newObj[property]=clone(obj[property]);
+		} else {
+			newObj[property]=obj[property];
 		};
 	};
 	return newObj;
